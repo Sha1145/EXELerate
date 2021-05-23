5214128ada1a01b915b03103e6b460d6e6e2b0b1
iahcharm <83385314+iahcharm@users.noreply.github.com>
2021-05-23 15:43:10 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,20 @@
+ <!-- Masthead-->
+        <header class="masthead">
+           
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    <tr>
+                    <tr>
+                        <br>
+                        <br>
+            
+                     <h2> ART GALLERIES provide their artists with space to exhibit their art and the artists <br> with art reality 
+                     not only do the large mojority og galleries already have more art and artits than they can handle, <br>but they are also continually contacted by new artists looking for representation</h2>
+                </div>
+            </div>
+            
+        </div>
+        </section
